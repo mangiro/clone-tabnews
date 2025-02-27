db8e28c812822da97eb5a7491cf6f10006503212
Gabriel Mangiro <gabrielmangi01@gmail.com>
2025-02-27 20:49:52 -0300

    Add `Prettier` config and lint scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Renata, você é o Wi-Fi do meu coração, Conectado em você, não quero mais nenhuma outra conexão! ❤️😄</h1>;
+  return (
+    <h1>
+      Renata, você é o Wi-Fi do meu coração, Conectado em você, não quero mais
+      nenhuma outra conexão! ❤️😄
+    </h1>
+  );
 }
 
 export default Home;
